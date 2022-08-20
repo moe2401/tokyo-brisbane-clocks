69ac67e2b3c1914ffd818ddd29320f360f42008f
moe <moe.aoki0124@gmail.com>
2022-08-20 16:03:34 +1000

    fixed functionality of clocks to show Tokyo time and Brisbane time.

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
-const secondsHand = document.getElementById("seconds-hand")
-const minutesHand = document.getElementById("minutes-hand")
-const hoursHand = document.getElementById("hours-hand")
+const tokyoSecondsHand = document.getElementById("tokyo-seconds-hand")
+const tokyoMinutesHand = document.getElementById("tokyo-minutes-hand")
+const tokyoHoursHand = document.getElementById("tokyo-hours-hand")
+
+const brisbaneSecondsHand = document.getElementById("brisbane-seconds-hand")
+const brisbaneMinutesHand = document.getElementById("brisbane-minutes-hand")
+const brisbaneHoursHand = document.getElementById("brisbane-hours-hand")
+
+
 
 function getTokyoTime() {
-    const now = new Date()
+    const now = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000))
     const seconds = now.getSeconds()
     const minutes = now.getMinutes()
     const hours = now.getHours()
     const timeInterval = 6
 
 
-    secondsHand.style.transform = "rotate(" + (seconds * timeInterval) + "deg)"
-    minutesHand.style.transform = "rotate(" + (minutes * timeInterval + seconds / 10) + "deg)"
-    hoursHand.style.transform = "rotate(" + (hours * 30 + minutes / 2) + "deg)"
+    tokyoSecondsHand.style.transform = "rotate(" + (seconds * timeInterval) + "deg)"
+    tokyoMinutesHand.style.transform = "rotate(" + (minutes * timeInterval + seconds / 10) + "deg)"
+    tokyoHoursHand.style.transform = "rotate(" + (hours * 30 + minutes / 2) + "deg)"
 }
 
 setInterval(getTokyoTime, 100)
+
+
+
+function getBrisbaneTime() {
+    const now = new Date(Date.now() + ((new Date().getTimezoneOffset() + (10 * 60)) * 60 * 1000))
+    const seconds = now.getSeconds()
+    const minutes = now.getMinutes()
+    const hours = now.getHours()
+    const timeInterval = 6
+
+
+    brisbaneSecondsHand.style.transform = "rotate(" + (seconds * timeInterval) + "deg)"
+    brisbaneMinutesHand.style.transform = "rotate(" + (minutes * timeInterval + seconds / 10) + "deg)"
+    brisbaneHoursHand.style.transform = "rotate(" + (hours * 30 + minutes / 2) + "deg)"
+}
+
+setInterval(getBrisbaneTime, 100)
+
